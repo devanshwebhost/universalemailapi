79f1e65ea26f54ae31a51ea102a91ef1cfb70755
Devansh Rajput <devanshrajput032006@gmail.com>
2025-06-28 19:30:37 +0530

    cors origin *

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 // CORS setup
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
